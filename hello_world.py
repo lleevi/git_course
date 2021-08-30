ec8ee0658a991fe3805da5594b6145a61ac2b215
lleevi <89691908+lleevi@users.noreply.github.com>
2021-08-31 02:15:53 +0300

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,6 @@
 print("Hello world!")
 <<<<<<< HEAD
-<<<<<<< HEAD
-print("Hello darkness my old friend")
-=======
 print("Hello from feature")
 >>>>>>> feature/create-awesome
-=======
 print("Hello from release")
 >>>>>>> release
